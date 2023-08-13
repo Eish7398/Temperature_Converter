6660d80ed22492c23fd83691a616b3dae81a84d3
INDRA SHEKHAR SINGH <122684130+Eish7398@users.noreply.github.com>
2023-08-13 13:47:33 +0100

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,26 @@
+
+        var cel = document.getElementById("cel");
+        var fah = document.getElementById("fah");
+
+        cel.addEventListener('input', function(){
+            console.log("cel changed");
+            let c = this.value;
+            let f = (c * 9/5) + 32;
+            if(!Number.isInteger(f)){
+                f = f.toFixed(4);
+            }
+            fah.value = f;
+
+        });
+
+        fah.addEventListener('input', function(){
+            console.log("fah changed");
+            let f = this.value;
+            let c = (f - 32) * 5/9;
+            if(!Number.isInteger(c)){
+                c = c.toFixed(4);
+            }
+            cel.value = c;
+
+        });
+    
